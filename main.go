0231a44cb50b799a25ed73c901e8c9374ef0c5e6
Aurino Geraldo <aurinoaj@gmail.com>
2022-08-23 11:56:48 -0300

    Create request http get

--- main.go
+++ main.go
@@ -2,30 +2,82 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"os/exec"
+	"strconv"
+	"time"
 )
 
-func main() {
-	var comando int
+const VERSION = "v1.0"
+const CICLES = 2
+const DELAY = 5
 
+func main() {
+	clearScreen()
+	fmt.Println("Este programa está na versão:", VERSION)
 	fmt.Println("Salve salve Aurigod")
 
-	fmt.Println("1- Iniciar Monitoramento")
+	for {
+		showMenu()
+
+		var comando int
+		fmt.Scan(&comando) // & serve para acessar o endereço de memoria.
+
+		switch comando {
+		case 1:
+			startingMonitoring()
+		case 2:
+			fmt.Println("\nExibindo logs...")
+		case 0:
+			fmt.Println("\nSaindo...")
+			os.Exit(0)
+		default:
+			fmt.Println("\nNão reconheço este comando.")
+		}
+	}
+}
+
+func showMenu() {
+	fmt.Println("\n1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
 
-	// & serve para acessar o endereço de memoria.
-	fmt.Scan(&comando)
+func clearScreen() {
+	cmd := exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
 
-	switch comando {
-	case 1:
-		fmt.Println("\nIniciando monitoramento...")
-	case 2:
-		fmt.Println("\nExibindo logs...")
-	case 0:
-		fmt.Println("\nSaindo...")
-	default:
-		fmt.Println("Não reconheço este comando.")
-		os.Exit(1)
+func startingMonitoring() {
+	clearScreen()
+	fmt.Println("\nIniciando monitoramento...")
+
+	sites := []string{"https://www.aurino.dev"}
+
+	for i := 0; i < CICLES; i++ {
+		for _, site := range sites {
+			fmt.Println("\nAcessando site:", site)
+
+			statusMessage := executeRequest(site)
+			fmt.Println(statusMessage)
+		}
+
+		time.Sleep(DELAY * time.Second)
 	}
+}
+
+func executeRequest(site string) string {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Vishi deu ruim erro:", err)
+	}
+
+	if resp.StatusCode == 200 {
+		return "Site " + site + " carregado com sucesso"
+	}
+
+	return "Site " + site + " está com problemas. status code:" + strconv.Itoa(resp.StatusCode)
 }
